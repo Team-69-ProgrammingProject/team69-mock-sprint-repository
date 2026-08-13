dd3ad04157a0eb5d1c65f9e6da9e417aa1e63411
Pema Tenzin <s4091921@student.rmit.edu.au>
2026-08-13 15:09:05 +1000

    feat: Implement team href in side bar + font update

--- frontend/src/app/(dashboard)/team/page.tsx
+++ frontend/src/app/(dashboard)/team/page.tsx
@@ -1,6 +1,18 @@
 import type { Metadata } from 'next'
 import { getServerSession } from '@/actions/auth.actions'
 import { redirect } from 'next/navigation'
+import { Fraunces } from 'next/font/google'
+import { Schibsted_Grotesk } from 'next/font/google'
+
+const fraunces = Fraunces({
+  subsets: ['latin'],
+  weight: ['700'],
+})
+const schibsted_grotesk = Schibsted_Grotesk({
+  subsets: ['latin'],
+  weight: ['400']
+  
+})
 
 
 interface TeamMember {
@@ -53,7 +65,7 @@ export default async function Team() {
         <div className="min-h-screen bg-[#FAF7F2] text-black">
             <div className="mx-auto">
                 <div className="text-center mb-12">
-                    <h1 className="text-4xl font-bold text-slate-900 pt-30 mb-2">
+                    <h1 className={`${fraunces.className} text-[38px] font-bold text-slate-900 pt-30 mb-2`}>
                         Meet the Team
                     </h1>
                 </div>
@@ -62,13 +74,13 @@ export default async function Team() {
                     {teamMembers.map((member) => (
                         <div
                             key={member.id}
-                            className="bg-[#FFFCFB] rounded-lg border-2 border-[#B0BEC5] p-6 flex flex-col items-center min-h-75 max-w-60 flex-shrink-0"
+                            className={`${schibsted_grotesk.className} text-[14px] bg-[#FFFCFB] rounded-lg border-2 border-[#B0BEC5] p-6 flex flex-col items-center h-[239px] w-[250px] flex-shrink-0`}
                         >
                             <div className="h-20 w-20 flex items-center justify-center mb-4">
                                 <img
                                     src="/userProfile.svg"
                                     alt="User Profile"
-                                    className="w-20 h-20"
+                                    className="w-[106px] h-[106px]"
                                 />
                             </div>
                             <h3 className="text-center text-sm font-medium text-[#2A1D19] mb-1">
