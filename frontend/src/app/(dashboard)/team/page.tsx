95761ceadea8be331383d4bab303c872cd2bf91b
Pema Tenzin <s4091921@student.rmit.edu.au>
2026-08-13 13:30:00 +1000

    feat: implement 5 boxs for member introduction

--- frontend/src/app/(dashboard)/team/page.tsx
+++ frontend/src/app/(dashboard)/team/page.tsx
@@ -49,5 +49,36 @@ export default async function Team(){
   if (!session) {
     redirect('/signin')
   }
+    return (
+    <div className="min-h-screen bg-[#FAF7F2] text-black">
+      <div className="max-w-7xl mx-auto">
+        <div className="text-center mb-12">
+          <h1 className="text-4xl font-bold text-slate-900 mb-2">
+            Meet the Team
+          </h1>
+        </div>
+
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-5 gap-6 mb-12 ">
+          {teamMembers.map((member) => (
+            <div
+              key={member.id}
+              className="bg-[#FFFCFB] rounded-lg border border-[#B0BEC5] border-[2px] p-6"
+            >
+              <h3 className="text-center text-sm font-medium text-[#2A1D19] mb-1">
+                {member.name}
+              </h3>
+
+              <p className="text-center text-xs font-medium text-[#2A1D19] mb-3">
+                {member.role}
+              </p>
+              <p className="text-center text-xs text-[#2A1D19] leading-relaxed">
+                {member.description}
+              </p>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
 
 }
